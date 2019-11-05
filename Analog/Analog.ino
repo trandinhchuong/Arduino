0b3ad7aadd7d59eaa3be722d6d94078cd3ea04ab
chuong <trandinhchuonght96@gmail.com>
2019-11-05 20:11:29 +0700

    doc gia tri bien tro

--- Analog/Analog.ino
+++ Analog/Analog.ino
@@ -1,15 +1,15 @@
-#define readPin A0
+#define readPin A0  //khai bao chan doc bien tro
 
-float Analog;
-float votal;
+float Analog;       //khai bao bien analog
+float votal;        //khai bao bien dien ap
 
 void setup() {
-Serial.begin(9600);
+Serial.begin(9600); //khai bao cong Serial toc do 9600
 }
 
 void loop() {
-Analog= analogRead(readPin);
-votal= Analog *  5.0 /1023.0;
+Analog= analogRead(readPin);   //doc gia tri analog 0-5v -> 0-1023
+votal= Analog *  5.0 /1023.0;  //doi analog qua nhiet do 
 Serial.print("Analog= \t");
 Serial.print(Analog);
 Serial.print(" votal= \t");
